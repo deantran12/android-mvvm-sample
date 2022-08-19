8234b703af180103343d6b988b5aa5ef942b4743
Son Tran <>
2022-08-19 11:22:54 +0700

    create project

---
+++ app/src/main/java/com/sonth/mvvm/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sonth.mvvm.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
